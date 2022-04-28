614a4748a98f2066d8c15cae7bc7044fbed4b09c
Soufiane Benounssi <soufiane.benounssi@gmail.com>
2022-04-28 08:55:45 -0400

    Initial commit

---
+++ code.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+function dark(){
+    let hdrColor = document.getElementById("nav_wrap") 
+    document.getElementById("home_bg").classList.toggle("hero_bg-dark")
+    hdrColor.classList.toggle("header_dark-nav")
+    document.getElementById("darkMode").classList.toggle("lightMode")
+}
+
