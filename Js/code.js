eaa3b071b1557263dd76c008712158a49978599a
Soufiane Benounssi <soufiane.benounssi@gmail.com>
2022-04-29 10:09:33 -0400

    update

--- Js/code.js
+++ Js/code.js
@@ -1,7 +1,16 @@
+
+
+window.addEventListener('scroll', function(){
+    var value = window.scrollY;
+
+    document.getElementById("home_bg").style.transform = `translateY(${value * .3}px)`;
+});
+
 function dark(){
     let hdrColor = document.getElementById("nav_wrap") 
     document.getElementById("home_bg").classList.toggle("hero_bg-dark")
     hdrColor.classList.toggle("header_dark-nav")
     document.getElementById("darkMode").classList.toggle("lightMode")
-}
+};
+
 
